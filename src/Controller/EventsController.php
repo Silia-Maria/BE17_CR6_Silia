40e16f75c03effd2503937b5615e86beaa2c9046
Silia Cronauer <silia.maria@web.de>
2022-12-02 14:44:03 +0100

    added hero & print dummy data

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -2,17 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\Events;
+use App\Entity\Organisers;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventsController extends AbstractController
 {
+    # index
     #[Route('/events', name: 'app_events')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $events = $doctrine->getRepository(Events::class)->findAll();
         return $this->render('events/index.html.twig', [
-            'controller_name' => 'EventsController',
+            'events' => $events,
         ]);
+    }
+
+    #create
+    #[Route('/create', name: 'create')]
+    public function create(): Response
+    {
+        return $this->render('events/create.html.twig', []);
+    }
+
+    #edit
+    #[Route('/edit/{id}', name: 'edit')]
+    public function edit($id): Response
+    {
+        return $this->render('events/edit.html.twig', []);
+    }
+
+    #details
+    #[Route('/details/{id}', name: 'details')]
+    public function details($id): Response
+    {
+        return $this->render('events/details.html.twig', []);
+    }
+
+    #delete
+    #[Route('/delete/{id}', name: 'detelete')]
+    public function delete($id): Response
+    {
+        return $this->render('events/delete.html.twig', []);
     }
 }
